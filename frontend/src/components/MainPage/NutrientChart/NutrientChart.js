85ba962d1e1cdc509695f5513f78e7467e5a3bd8
hallov012 <hallov012@gmail.com>
2022-09-22 17:57:15 +0900

    upload

--- frontend/src/components/MainPage/NutrientChart/NutrientChart.js
+++ frontend/src/components/MainPage/NutrientChart/NutrientChart.js
@@ -1,6 +1,10 @@
 import classes from "./NutrientChart.module.css"
 
 function NutrientChart() {
-  return <div className={classes.chart_box}>섭취 영양소 비율</div>
+  return (
+    <div className={classes.chart_box}>
+      <span>섭취 영양소 비율</span>
+    </div>
+  )
 }
 export default NutrientChart
