caa29d205397b24bf8c411f17e9736427a288483
KONKUK\s_yjs010604 <didnlie@gmail.com>
2022-09-23 13:56:07 +0900

    FEAT: Feat AddPage pages AddBasketPage 구현

--- frontend/src/components/AddPage/pages/AddBasketPage.js
+++ frontend/src/components/AddPage/pages/AddBasketPage.js
@@ -1,5 +1,34 @@
+import classes from "./AddBasketPage.module.css";
+import AddButtonList from "../AddButtonList/AddButtonList";
+import TopNav from "../../TopNav/TopNav";
+
 function AddBasketPage() {
-  return <div>basket</div>;
+  return (
+    <div>
+      <div id="top_nav_area">
+        <TopNav arrow={["/signup/gender", ""]}></TopNav>
+        {/* 링크 작업 해야됨 */}
+      </div>
+      <div className={classes.container}>
+        <div>
+          <div className={classes.title}>추가될 음식</div>
+          <div className={classes.item}>
+            <div className={classes.itemInfo}>
+              <div className={classes.name}>삶은 계란</div>
+              <div className={classes.amount}>1인분</div>
+            </div>
+            <div className={classes.calorie}>63kcal</div>
+          </div>
+        </div>
+        <div className={classes.sum}>
+          <div>합계</div>
+          <div>360kcal</div>
+        </div>
+      </div>
+
+      <AddButtonList></AddButtonList>
+    </div>
+  );
 }
 
 export default AddBasketPage;
