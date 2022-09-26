e5aa5e330c886ef917a16e855aee6a72e929b36a
expertinpyo <ypd02363@naver.com>
2022-09-26 16:19:02 +0900

    FEAT:Create Food PUT and DELETE API

--- backend/src/main/java/com/easylose/backend/api/v1/controller/FoodController.java
+++ backend/src/main/java/com/easylose/backend/api/v1/controller/FoodController.java
@@ -11,8 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,5 +45,21 @@ public class FoodController {
       @AuthenticationPrincipal Long id, @RequestBody FoodUserDto dto) {
     FoodResponseDto response = foodService.createFood(id, dto);
     return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+
+  @PutMapping("{food_id}")
+  @Operation(summary = "유저 커스텀 음식 수정", description = "유저의 커스텀 음식을 수정한다")
+  public ResponseEntity<FoodResponseDto> updateFood(
+      @AuthenticationPrincipal Long id, @PathVariable Long food_id, @RequestBody FoodUserDto dto) {
+
+    FoodResponseDto response = foodService.updateFood(id, food_id, dto);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+
+  @DeleteMapping("{food_id}")
+  @Operation(summary = "유저 커스텀 음식 삭제", description = "요청을 보내는 유저의 커스텀 음식을 삭제한다")
+  public ResponseEntity deleteFood(@AuthenticationPrincipal Long id, @PathVariable Long food_id) {
+    foodService.deleteFood(id, food_id);
+    return ResponseEntity.status(HttpStatus.OK).body(null);
   }
 }
--- backend/src/main/java/com/easylose/backend/api/v1/mapper/FoodMapper.java
+++ backend/src/main/java/com/easylose/backend/api/v1/mapper/FoodMapper.java
@@ -3,7 +3,11 @@ package com.easylose.backend.api.v1.mapper;
 import com.easylose.backend.api.v1.domain.Food;
 import com.easylose.backend.api.v1.dto.FoodDto.FoodResponseDto;
 import com.easylose.backend.api.v1.dto.FoodDto.FoodUserDto;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -13,4 +17,9 @@ public interface FoodMapper {
   Food toEntity(final FoodUserDto dto);
 
   FoodResponseDto toDto(final Food food);
+
+  @BeanMapping(
+      nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+      nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+  void updateFoodFromDto(FoodUserDto dto, @MappingTarget Food food);
 }
