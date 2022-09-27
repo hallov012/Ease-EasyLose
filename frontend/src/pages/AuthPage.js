a12e31a9e41ce961f1c00bd2a7dcaf0d793f20df
Hyelim <hallov012@gmail.com>
2022-09-27 15:30:51 +0900

    Fix: userBirth to userAge

--- frontend/src/pages/AuthPage.js
+++ frontend/src/pages/AuthPage.js
@@ -1,26 +1,26 @@
-import { useLocation } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { registerAccessToken, registerUserInfo } from "../store/userSlice";
-import { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
+import { useLocation } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { registerAccessToken, registerUserInfo } from "../store/userSlice"
+import { useEffect, useState } from "react"
+import { useHistory } from "react-router-dom"
+import axios from "axios"
 
 function AuthPage() {
-  const location = useLocation();
-  const history = useHistory();
-  const dispatch = useDispatch();
+  const location = useLocation()
+  const history = useHistory()
+  const dispatch = useDispatch()
 
   const tokens = location.search
     .replace("?accessToken=", "")
     .replace("refreshToken=", "")
-    .split("&");
+    .split("&")
 
-  const accessToken = useSelector((state) => state.user.accessToken);
-  const userInfo = useSelector((state) => state.user.userInfo);
+  const accessToken = useSelector((state) => state.user.accessToken)
+  const userInfo = useSelector((state) => state.user.userInfo)
 
   useEffect(() => {
-    dispatch(registerAccessToken(tokens));
-  }, [tokens, dispatch]);
+    dispatch(registerAccessToken(tokens))
+  }, [tokens, dispatch])
 
   useEffect(() => {
     if (accessToken) {
@@ -29,25 +29,25 @@ function AuthPage() {
           headers: { Authorization: `Bearer ${accessToken}` },
         })
         .then((response) => {
-          dispatch(registerUserInfo(response.data));
+          dispatch(registerUserInfo(response.data))
         })
         .catch((error) => {
-          console.log(error);
-        });
+          console.log(error)
+        })
     }
-  }, [accessToken, dispatch]);
+  }, [accessToken, dispatch])
 
   useEffect(() => {
     if (userInfo) {
       if (userInfo.goal) {
-        history.push("/main");
+        history.push("/main")
       } else {
-        history.push("/signup/gender");
+        history.push("/signup/gender")
       }
     }
-  }, [userInfo, dispatch, history]);
+  }, [userInfo, dispatch, history])
 
-  return <div></div>;
+  return <div></div>
 }
 
-export default AuthPage;
+export default AuthPage
--- frontend/src/pages/SignUpPage.js
+++ frontend/src/pages/SignUpPage.js
@@ -1,29 +1,29 @@
-import TopNav from "../components/TopNav/TopNav";
-import { Route, Switch, useLocation } from "react-router-dom";
-import SignUpHeader from "../components/SignUpPage/SignUpHeader/SignUpHeader";
-import { useEffect, useState } from "react";
-import GenderPicker from "../components/SignUpPage/GenderPicker/GenderPicker";
-import BirthPicker from "../components/SignUpPage/BirthPicker/BirthPicker";
-import HeightPicker from "../components/SignUpPage/HeightPicker/HeightPicker";
-import WeightPicker from "../components/SignUpPage/WeightPicker/WeightPicker";
-import ActivityPicker from "../components/SignUpPage/ActivityPicker/ActivityPicker";
-import GoalPicker from "../components/SignUpPage/GoalPicker/GoalPicker";
-import PickComplete from "../components/SignUpPage/PickComplete/PickComplete";
-import { useDispatch, useSelector } from "react-redux";
-import { registerUserInfo } from "../store/userSlice";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
+import TopNav from "../components/TopNav/TopNav"
+import { Route, Switch, useLocation } from "react-router-dom"
+import SignUpHeader from "../components/SignUpPage/SignUpHeader/SignUpHeader"
+import { useEffect, useState } from "react"
+import GenderPicker from "../components/SignUpPage/GenderPicker/GenderPicker"
+import BirthPicker from "../components/SignUpPage/BirthPicker/BirthPicker"
+import HeightPicker from "../components/SignUpPage/HeightPicker/HeightPicker"
+import WeightPicker from "../components/SignUpPage/WeightPicker/WeightPicker"
+import ActivityPicker from "../components/SignUpPage/ActivityPicker/ActivityPicker"
+import GoalPicker from "../components/SignUpPage/GoalPicker/GoalPicker"
+import PickComplete from "../components/SignUpPage/PickComplete/PickComplete"
+import { useDispatch, useSelector } from "react-redux"
+import { registerUserInfo } from "../store/userSlice"
+import { useHistory } from "react-router-dom"
+import axios from "axios"
 function SignUpPage() {
-  const location = useLocation().pathname.replace("/signup", "");
-  const [userGender, setUserGender] = useState("");
-  const [userBirth, setUserBirth] = useState("20000101");
-  const [userWeight, setUserWeight] = useState(70);
-  const [userHeight, setUserHeight] = useState(180);
-  const [userActivity, setUserActivity] = useState(5);
-  const [userGoal, setUserGoal] = useState(5);
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const accessToken = useSelector((state) => state.user.accessToken);
+  const location = useLocation().pathname.replace("/signup", "")
+  const [userGender, setUserGender] = useState("")
+  const [userAge, setUserAge] = useState(20)
+  const [userWeight, setUserWeight] = useState(70)
+  const [userHeight, setUserHeight] = useState(180)
+  const [userActivity, setUserActivity] = useState(5)
+  const [userGoal, setUserGoal] = useState(5)
+  const history = useHistory()
+  const dispatch = useDispatch()
+  const accessToken = useSelector((state) => state.user.accessToken)
 
   function putUserInfo() {
     axios
@@ -35,7 +35,7 @@ function SignUpPage() {
           goal: userGoal,
           weight: userWeight,
           height: userHeight,
-          birthdate: userBirth,
+          age: userAge,
           isAutomatic: true,
         },
         {
@@ -50,50 +50,50 @@ function SignUpPage() {
             Authorization: `Bearer ${accessToken}`,
           },
         }).then((response) => {
-          dispatch(registerUserInfo(response.data));
-        });
-        history.push("/main");
+          dispatch(registerUserInfo(response.data))
+        })
+        history.push("/main")
       })
       .catch((error) => {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
 
   console.log(
-    `${userGender}/${userBirth}/${userWeight}kg/${userHeight}cm/${userActivity}/${userGoal}`
-  );
+    `${userGender}/${userAge}/${userWeight}kg/${userHeight}cm/${userActivity}/${userGoal}`
+  )
 
-  const informationMap = new Map();
+  const informationMap = new Map()
   informationMap.set("/gender", {
     title: "성별",
     subtitle: "맞춤 추천을 위한 정보를 요청드립니다!",
     done: "0",
-  });
-  informationMap.set("/birth", {
-    title: "생일",
+  })
+  informationMap.set("/age", {
+    title: "나이",
     subtitle: "맞춤 추천을 위한 정보를 요청드립니다!",
     done: "16",
-  });
+  })
   informationMap.set("/height", {
     title: "키",
     subtitle: "맞춤 추천을 위한 정보를 요청드립니다!",
     done: "32",
-  });
+  })
   informationMap.set("/weight", {
     title: "몸무게",
     subtitle: "맞춤 추천을 위한 정보를 요청드립니다!",
     done: "48",
-  });
+  })
   informationMap.set("/activity", {
     title: "활동량",
     subtitle: "하루 활동량을 기준으로 선택해주세요!",
     done: "64",
-  });
+  })
   informationMap.set("/goal", {
     title: "목표",
     subtitle: "선택하실 목표에 맞게 영양 비율을 추천해드립니다!",
     done: "80",
-  });
+  })
   // informationMap.set("/verify", {
   //   title: "입력 확인",
   //   subtitle: "보다 정확한 추천을 위해 입력 내역을 확인해주세요!",
@@ -104,40 +104,40 @@ function SignUpPage() {
     title: "수고하셨습니다!",
     subtitle: "",
     done: "100",
-  });
+  })
 
   function setArrow() {
     if (location === "/gender") {
       if (userGender === "") {
-        return ["", ""];
+        return ["", ""]
       } else {
-        return ["", "/signup/birth"];
+        return ["", "/signup/age"]
       }
     } else if (location === "/activity") {
       if (userActivity === 5) {
-        return ["/signup/weight", ""];
+        return ["/signup/weight", ""]
       } else {
-        return ["/signup/weight", "/signup/goal"];
+        return ["/signup/weight", "/signup/goal"]
       }
     } else if (location === "/goal") {
       if (userGoal === 5) {
-        return ["/signup/activity", ""];
+        return ["/signup/activity", ""]
       } else {
-        return ["/signup/activity", "/signup/complete"];
+        return ["/signup/activity", "/signup/complete"]
       }
     } else if (location === "/height") {
-      return ["/signup/birth", "/signup/weight"];
+      return ["/signup/age", "/signup/weight"]
     } else if (location === "/weight") {
-      return ["/signup/height", "/signup/activity"];
-    } else if (location === "/birth") {
-      return ["/signup/gender", "/signup/height"];
+      return ["/signup/height", "/signup/activity"]
+    } else if (location === "/age") {
+      return ["/signup/gender", "/signup/height"]
     } else {
-      return ["", ""];
+      return ["", ""]
     }
   }
 
   function renderSignUpPage() {
-    const temp = informationMap.get(location);
+    const temp = informationMap.get(location)
     return (
       <div>
         <div id="top_nav_area">
@@ -154,7 +154,7 @@ function SignUpPage() {
               <GenderPicker
                 value={userGender}
                 setGender={(value) => {
-                  setUserGender(value);
+                  setUserGender(value)
                   // setUserInfo((current) => {
                   //   let newObject = { ...current };
                   //   newObject["gender"] = value;
@@ -167,7 +167,7 @@ function SignUpPage() {
               <HeightPicker
                 value={userHeight}
                 setHeight={(value) => {
-                  setUserHeight(value);
+                  setUserHeight(value)
                 }}
               ></HeightPicker>
             </Route>
@@ -175,28 +175,28 @@ function SignUpPage() {
               <WeightPicker
                 value={userWeight}
                 setWeight={(value) => {
-                  setUserWeight(value);
+                  setUserWeight(value)
                 }}
               ></WeightPicker>
             </Route>
-            <Route path="/signup/birth">
-              <BirthPicker
+            <Route path="/signup/age">
+              {/* <BirthPicker
                 value={userBirth}
                 setBirth={(value) => {
-                  setUserBirth(value);
+                  setUserBirth(value)
                   // setUserInfo((current) => {
                   //   let newObject = { ...current };
                   //   newObject["birth"] = value;
                   //   return newObject;
                   // });
                 }}
-              ></BirthPicker>
+              ></BirthPicker> */}
             </Route>
             <Route path="/signup/activity">
               <ActivityPicker
                 value={userActivity}
                 setValue={(value) => {
-                  setUserActivity(value);
+                  setUserActivity(value)
                 }}
               ></ActivityPicker>
             </Route>
@@ -204,24 +204,24 @@ function SignUpPage() {
               <GoalPicker
                 value={userGoal}
                 setValue={(value) => {
-                  setUserGoal(value);
+                  setUserGoal(value)
                 }}
               ></GoalPicker>
             </Route>
             <Route path="/signup/complete">
               <PickComplete
                 putUserInfo={() => {
-                  putUserInfo();
+                  putUserInfo()
                 }}
               ></PickComplete>
             </Route>
           </Switch>
         </div>
       </div>
-    );
+    )
   }
 
-  return <div>{renderSignUpPage()}</div>;
+  return <div>{renderSignUpPage()}</div>
 }
 
-export default SignUpPage;
+export default SignUpPage
