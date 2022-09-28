07fcd5d433093278fcccbd9f2b4b897c82a5ded1
KONKUK\s_yjs010604 <didnlie@gmail.com>
2022-09-28 15:58:57 +0900

    FIX: Fix axios bug

--- frontend/src/pages/PlanPage.js
+++ frontend/src/pages/PlanPage.js
@@ -1,15 +1,5 @@
-import TopNavDate from "../components/TopNav/TopNavDate"
-
 function PlanPage() {
-  return (
-    <div>
-      <div id="top_nav_area">
-        {/* <TopNav text={"2022.09.15 (목)"} arrow={[0, 1]} /> */}
-        {/* <TopNavDate /> */}
-      </div>
-      <h1>Welcome to PlanPage</h1>
-    </div>
-  )
+  return <div></div>
 }
 
 export default PlanPage
