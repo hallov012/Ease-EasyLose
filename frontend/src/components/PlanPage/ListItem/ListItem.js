4d4422e8a8d45277273133372131b5b2f71ff1cc
KONKUK\s_yjs010604 <didnlie@gmail.com>
2022-09-28 17:27:41 +0900

    FEAT: Feat ListItem

---
+++ frontend/src/components/PlanPage/ListItem/ListItem.js
@@ -0,0 +1,24 @@
+import classes from "./ListItem.module.css"
+
+function ListItem() {
+  return (
+    <div className={classes.container}>
+      <div className={classes.left}>
+        <div className={classes.item_title}>Feliz Navidata</div>
+        <div className={classes.item_subtitle}>
+          Jose Feliciano Christmas Hitsfdkljfsdakljfasdkjlafdskjl
+        </div>
+      </div>
+      <div className={classes.right}>
+        <div>
+          <i className="fa-regular fa-circle-question"></i>
+        </div>
+        <div>
+          <i className="fa-regular fa-square-plus"></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
---
+++ frontend/src/components/PlanPage/Pages/PlanMainPage.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import SelectBtn from "../../ChartPage/SelectBtn/SelectBtn"
+import ListItem from "../ListItem/ListItem"
+import ListItemDelete from "../ListItemDelete/ListItemDelete"
+import classes from "./PlanMainPage.module.css"
+
+function PlanMainPage() {
+  const [term, setTerm] = useState(0)
+  const dispatch = useDispatch()
+  //   const oneMealList = useSelector((state) => state.plan.oneMealList)
+  //   const dailyMealList = useSelector((state) => state.plan.dailyMealList)
+
+  const oneMealList = [1, 2]
+  const dailyMealList = [1, 2]
+
+  useEffect(() => {}, [])
+
+  return (
+    <div className={classes.container}>
+      <div style={{ width: "90vw" }}>
+        <SelectBtn
+          data={["한끼 모음", "하루 식단 모음"]}
+          setValue={(value) => {
+            setTerm(value)
+          }}
+        ></SelectBtn>
+      </div>
+      <div className={classes.itemContainer}>
+        {term === 0
+          ? oneMealList.map((item) => {
+              return (
+                <div style={{ width: "90vw", height: "10vh" }}>
+                  <ListItem></ListItem>
+                </div>
+              )
+            })
+          : dailyMealList.map((item) => {
+              return (
+                <div style={{ width: "90vw", height: "10vh" }}>
+                  <ListItemDelete></ListItemDelete>
+                </div>
+              )
+            })}
+      </div>
+    </div>
+  )
+}
+
+export default PlanMainPage
