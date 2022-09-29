69d2a3d54845115e72939bf2ba67ed3675471331
tokataka <skygazer227@gmail.com>
2022-09-29 16:39:11 +0900

    FEAT: Add calendar endpoint

--- backend/src/main/java/com/easylose/backend/api/v1/repository/MeasureLogRepository.java
+++ backend/src/main/java/com/easylose/backend/api/v1/repository/MeasureLogRepository.java
@@ -1,8 +1,15 @@
 package com.easylose.backend.api.v1.repository;
 
 import com.easylose.backend.api.v1.domain.MeasureLog;
+import com.easylose.backend.api.v1.domain.User;
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface MeasureLogRepository
-    extends JpaRepository<MeasureLog, Long>, JpaSpecificationExecutor<MeasureLog> {}
+    extends JpaRepository<MeasureLog, Long>, JpaSpecificationExecutor<MeasureLog> {
+
+  MeasureLog findTopByUserAndCreatedAtLessThanOrderByCreatedAtDesc(User user, LocalDateTime date);
+
+  MeasureLog findTopByUser(User user);
+}
---
+++ backend/src/main/java/com/easylose/backend/api/v1/service/CalendarService.java
@@ -0,0 +1,9 @@
+package com.easylose.backend.api.v1.service;
+
+import com.easylose.backend.api.v1.dto.CalendarDto.CalendarResponseDto;
+import java.time.YearMonth;
+import java.util.List;
+
+public interface CalendarService {
+  List<CalendarResponseDto> getCalendar(Long id, YearMonth date);
+}
