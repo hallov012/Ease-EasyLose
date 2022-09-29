e59504c03ab2685a0ec63dc23bcb476df1492718
Hyelim <hallov012@gmail.com>
2022-09-30 00:14:08 +0900

    Feat: signup complete data 유저 추천 영양 비율 반영

--- frontend/src/components/SignUpPage/PickComplete/PickComplete.js
+++ frontend/src/components/SignUpPage/PickComplete/PickComplete.js
@@ -11,30 +11,31 @@ function PickComplete({ putUserInfo }) {
     putUserInfo()
   })
   const userInfo = useSelector((state) => state.user.userInfo)
-  console.log(userInfo)
   return (
     <div className={classes.container}>
       <FontAwesomeIcon icon={faUserCheck} size="5x"></FontAwesomeIcon>
       <div>가입이 완료 되었습니다!</div>
       <div>다음과 같이 일일 영양소가 추천되었습니다.</div>
+      { userInfo ?
       <div className={classes.itemList}>
         <div className={classes.item}>
           <div>칼로리</div>
-          <div>1500kcal</div>
+          <div>{userInfo.dailyCalorie}cal</div>
         </div>
         <div className={classes.item}>
           <div>탄수화물</div>
-          <div>40g</div>
+          <div>{userInfo.dailyCarb}g</div>
         </div>
         <div className={classes.item}>
           <div>단백질</div>
-          <div>50g</div>
+          <div>{userInfo.dailyProtein}g</div>
         </div>
         <div className={classes.item}>
           <div>지방</div>
-          <div>30g</div>
+          <div>{userInfo.dailyFat}g</div>
         </div>
       </div>
+      : null}
       <div>
         <div className={classes.button} onClick={() => {}}>
           일일 영양소 수정하기
