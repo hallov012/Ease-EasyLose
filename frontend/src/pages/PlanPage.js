234a355169ebad5f73afd8472080c6eeb11243f2
Hyelim <hallov012@gmail.com>
2022-10-01 02:16:58 +0900

    error

--- frontend/src/pages/PlanPage.js
+++ frontend/src/pages/PlanPage.js
@@ -1,20 +1,20 @@
-import { useEffect } from "react"
-import { Route } from "react-router-dom"
-import PlanMainPage from "../components/PlanPage/Pages/PlanMainPage"
-import { instance } from "../api/index"
-import { useDispatch } from "react-redux"
-import { registerDailyMealList, registerOneMealList } from "../store/planSlice"
+import { useEffect } from "react";
+import { Route } from "react-router-dom";
+import PlanMainPage from "../components/PlanPage/Pages/PlanMainPage";
+import { instance } from "../api/index";
+import { useDispatch } from "react-redux";
+import { registerDailyMealList, registerOneMealList } from "../store/planSlice";
 
 function PlanPage() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     instance
       .get("/foodset", {})
       .then((response) => {
-        dispatch(registerDailyMealList(response.data))
+        dispatch(registerDailyMealList(response.data));
       })
-      .catch((error) => console.log(error))
-  }, [])
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div>
@@ -22,7 +22,7 @@ function PlanPage() {
         <PlanMainPage></PlanMainPage>
       </Route>
     </div>
-  )
+  );
 }
 
-export default PlanPage
+export default PlanPage;
