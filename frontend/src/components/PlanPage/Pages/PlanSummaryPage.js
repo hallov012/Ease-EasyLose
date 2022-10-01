d9cfce71f5805609bbc3a20a4b4592ebcc6b301e
Hyelim <hallov012@gmail.com>
2022-10-01 20:49:16 +0900

    Feat: planpage DailySummaryPage data -plan summary 페이지 데이터 연동

--- frontend/src/components/PlanPage/Pages/PlanSummaryPage.js
+++ frontend/src/components/PlanPage/Pages/PlanSummaryPage.js
@@ -1,8 +1,68 @@
+import TopHistoryNav from "../../TopNav/TopHistoryNav";
+import NutrientChart from "../../MainPage/NutrientChart/NutrientChart";
+import NutrientProgressGraph from "../../MainPage/NutrientProgressGraph/NutrientProgressGraph";
+import NutrientProgressBox from "../../MainPage/NutrientProgressBox/NutrientProgressBox";
+import UserInfoBox from "../../MainPage/UserInfoBox/UserInfoBox";
+
+import classes from "./PlanPage.module.css";
+
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { registerPlanId } from "../../../store/planSlice";
+
 function PlanSummaryPage() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const temp = JSON.parse(localStorage.getItem("target_date"));
+  if (temp) {
+    if (typeof temp === "number") {
+      dispatch(registerPlanId(temp));
+    }
+  } else {
+    history.push("/plan");
+  }
+
+  const userInfo = useSelector((state) => state.user.userInfo);
+  const planId = useSelector((state) => state.plan.planId);
+  const dailyMealList = useSelector((state) => state.plan.dailyMealList);
+  const [dailyMeal, setDailyMeal] = useState([]);
+  function isPlan(element) {
+    if (element.id === planId) {
+      return true;
+    }
+  }
+  useEffect(() => {
+    if (planId && dailyMealList.length) {
+      setDailyMeal(dailyMealList.filter(isPlan));
+    }
+  }, [planId, dailyMealList]);
+
   return (
-    <div>
-      <div>서머리</div>
-      <div>서머리</div>
+    <div className={classes.plan_page}>
+      {dailyMeal.length ? (
+        <div>
+          <div id="top_nav_area">
+            <TopHistoryNav text={`${dailyMeal[0].name}`} />
+          </div>
+          <div
+            style={{
+              margin: "10vh 5vw 15vh",
+            }}
+          >
+            <NutrientChart dietSum={dailyMeal[0].total} />
+            <NutrientProgressGraph
+              userInfo={userInfo}
+              dietSum={dailyMeal[0].total}
+            />
+            <NutrientProgressBox
+              userInfo={userInfo}
+              dietSum={dailyMeal[0].total}
+            />
+            <UserInfoBox userInfo={userInfo} />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
