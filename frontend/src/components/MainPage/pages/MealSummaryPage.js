57a0360607f4d562a92b49ff3b1c1935ddf955c5
Hyelim <hallov012@gmail.com>
2022-10-03 17:17:10 +0900

    Design: Meal Summary background -끼니별 아이콘 출력

--- frontend/src/components/MainPage/pages/MealSummaryPage.js
+++ frontend/src/components/MainPage/pages/MealSummaryPage.js
@@ -1,52 +1,55 @@
-import TopNav from "../../TopNav/TopNav"
-import MealNutrientInfo from "../MealNutrientInfo/MealNutrientInfo"
-import UserFoodList from "../UserFoodList/UserFoodList"
-import NutrientChart from "../NutrientChart/NutrientChart"
-import NutrientProgressBox from "../NutrientProgressBox/NutrientProgressBox"
+import classes from "./MealSummary.module.css";
+import TopNav from "../../TopNav/TopNav";
+import MealNutrientInfo from "../MealNutrientInfo/MealNutrientInfo";
+import UserFoodList from "../UserFoodList/UserFoodList";
+import NutrientChart from "../NutrientChart/NutrientChart";
+import NutrientProgressBox from "../NutrientProgressBox/NutrientProgressBox";
 
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
-import { registerLastEntered } from "../../../store/statusSlice"
-import { useDispatch, useSelector } from "react-redux"
-import { useState, useEffect } from "react"
+import { registerLastEntered } from "../../../store/statusSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
 function MealSummaryPage(props) {
-  const userInfo = useSelector((state) => state.user.userInfo)
-  const userDailyDiet = useSelector((state) => state.daily.dailyDiet)
-
+  const userInfo = useSelector((state) => state.user.userInfo);
+  const userDailyDiet = useSelector((state) => state.daily.dailyDiet);
   const [value, setValue] = useState({
     dietSum: undefined,
     foodList: undefined,
-  })
+  });
   // console.log(props.userDailyDiet)
 
-  const params = useParams()
+  const params = useParams();
   const meal = {
-    BREAKFAST: "아침",
-    LUNCH: "점심",
-    DINNER: "저녁",
-    SNACK: "간식",
-  }
-  const dispatch = useDispatch()
+    BREAKFAST: { title: "아침", icon: "fa-cloud-sun" },
+    LUNCH: { title: "점심", icon: "fa-sun" },
+    DINNER: { title: "저녁", icon: "fa-moon" },
+    SNACK: { title: "간식", icon: "fa-cookie-bite" },
+  };
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    localStorage.setItem("mealtime", params.mealtime)
-    dispatch(registerLastEntered(params.mealtime))
-  }, [params, dispatch])
+    localStorage.setItem("mealtime", params.mealtime);
+    dispatch(registerLastEntered(params.mealtime));
+  }, [params, dispatch]);
 
   useEffect(() => {
     if (userDailyDiet) {
-      const temp = { dietSum: undefined, foodList: undefined }
-      temp.dietSum = userDailyDiet[0].sums[params.mealtime]
-      temp.foodList = userDailyDiet[0].details[params.mealtime]
-      setValue(temp)
+      const temp = { dietSum: undefined, foodList: undefined };
+      temp.dietSum = userDailyDiet[0].sums[params.mealtime];
+      temp.foodList = userDailyDiet[0].details[params.mealtime];
+      setValue(temp);
     }
-  }, [userDailyDiet])
+  }, [userDailyDiet]);
 
   return (
     <div>
       <div id="top_nav_area">
-        <TopNav text={meal[params.mealtime]} arrow={["/main", ""]} />
+        <TopNav text={meal[params.mealtime].title} arrow={["/main", ""]} />
+      </div>
+      <div className={classes.icon}>
+        <i className={`fa-solid ${meal[params.mealtime].icon}`}></i>
       </div>
       <div
         style={{
@@ -63,6 +66,6 @@ function MealSummaryPage(props) {
         />
       </div>
     </div>
-  )
+  );
 }
-export default MealSummaryPage
+export default MealSummaryPage;
--- frontend/src/components/PlanPage/Pages/PlanMealSummaryPage.js
+++ frontend/src/components/PlanPage/Pages/PlanMealSummaryPage.js
@@ -1,66 +1,69 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useHistory } from "react-router-dom"
-import { registerPlanId } from "../../../store/planSlice"
-import { useParams } from "react-router-dom"
-import { registerLastEntered } from "../../../store/statusSlice"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { registerPlanId } from "../../../store/planSlice";
+import { useParams } from "react-router-dom";
+import { registerLastEntered } from "../../../store/statusSlice";
 
-import classes from "./PlanPage.module.css"
+import classes from "./PlanPage.module.css";
 
-import TopHistoryNav from "../../TopNav/TopHistoryNav"
-import MealNutrientInfo from "../../MainPage/MealNutrientInfo/MealNutrientInfo"
-import UserFoodList from "../../MainPage/UserFoodList/UserFoodList"
-import NutrientChart from "../../MainPage/NutrientChart/NutrientChart"
-import NutrientProgressBox from "../../MainPage/NutrientProgressBox/NutrientProgressBox"
+import TopHistoryNav from "../../TopNav/TopHistoryNav";
+import MealNutrientInfo from "../../MainPage/MealNutrientInfo/MealNutrientInfo";
+import UserFoodList from "../../MainPage/UserFoodList/UserFoodList";
+import NutrientChart from "../../MainPage/NutrientChart/NutrientChart";
+import NutrientProgressBox from "../../MainPage/NutrientProgressBox/NutrientProgressBox";
 
 function PlanMealSummaryPage({ colorSet }) {
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const temp = JSON.parse(localStorage.getItem("target_date"))
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const temp = JSON.parse(localStorage.getItem("target_date"));
   useEffect(() => {
     if (temp) {
       if (typeof temp === "number") {
-        dispatch(registerPlanId(temp))
+        dispatch(registerPlanId(temp));
       }
     } else {
-      history.push("/plan")
+      history.push("/plan");
     }
-  }, [])
+  }, []);
 
-  const userInfo = useSelector((state) => state.user.userInfo)
-  const planId = useSelector((state) => state.plan.planId)
-  const dailyMealList = useSelector((state) => state.plan.dailyMealList)
-  const [dailyMeal, setDailyMeal] = useState([])
+  const userInfo = useSelector((state) => state.user.userInfo);
+  const planId = useSelector((state) => state.plan.planId);
+  const dailyMealList = useSelector((state) => state.plan.dailyMealList);
+  const [dailyMeal, setDailyMeal] = useState([]);
   function isPlan(element) {
     if (element.id === planId) {
-      return true
+      return true;
     }
   }
   useEffect(() => {
     if (planId && dailyMealList.length) {
-      setDailyMeal(dailyMealList.filter(isPlan))
+      setDailyMeal(dailyMealList.filter(isPlan));
     }
-  }, [planId, dailyMealList])
+  }, [planId, dailyMealList]);
 
-  const params = useParams()
+  const params = useParams();
   const meal = {
-    BREAKFAST: "아침",
-    LUNCH: "점심",
-    DINNER: "저녁",
-    SNACK: "간식",
-  }
+    BREAKFAST: { title: "아침", icon: "fa-cloud-sun" },
+    LUNCH: { title: "점심", icon: "fa-sun" },
+    DINNER: { title: "저녁", icon: "fa-moon" },
+    SNACK: { title: "간식", icon: "fa-cookie-bite" },
+  };
 
   useEffect(() => {
-    localStorage.setItem("mealtime", params.mealtime)
-    dispatch(registerLastEntered(params.mealtime))
-  }, [params, dispatch])
+    localStorage.setItem("mealtime", params.mealtime);
+    dispatch(registerLastEntered(params.mealtime));
+  }, [params, dispatch]);
 
   return (
     <div className={classes.plan_page}>
       {dailyMeal.length ? (
         <div>
           <div id="top_nav_area">
-            <TopHistoryNav text={`${meal[params.mealtime]}`} />
+            <TopHistoryNav text={`${meal[params.mealtime].title}`} />
+          </div>
+          <div className={classes.icon}>
+            <i className={`fa-solid ${meal[params.mealtime].icon}`}></i>
           </div>
           <div
             style={{
@@ -82,6 +85,6 @@ function PlanMealSummaryPage({ colorSet }) {
         </div>
       ) : null}
     </div>
-  )
+  );
 }
-export default PlanMealSummaryPage
+export default PlanMealSummaryPage;
